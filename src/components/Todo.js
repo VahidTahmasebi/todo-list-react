332294aad7599d7cf45863da720a17f3fc6a292e
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2022-12-29 19:27:46 +0330

    Fix props complete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,10 +3,10 @@
 const Todo = ({ todo, onComplete }) => {
   return (
     <div className='todo'>
-      <div>{todo.text}</div>
+      <div onClick={onComplete}>{todo.text}</div>
       <div>
         <button>Edit</button>
-        <button onClick={onComplete}>Complete</button>
+        <button>Delete</button>
       </div>
     </div>
   );

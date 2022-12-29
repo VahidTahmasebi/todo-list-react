f92c6aba66bbfa6362fb4962610a08452b48ae06
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2022-12-29 17:00:19 +0330

    Create a new todo attribute

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,10 +3,22 @@ import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
 const TodoApp = () => {
-  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState("");
+
+  //   create a new todo attribute
+  const addTodoHandler = (input) => {
+    const newTodo = {
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+      isCompleted: false,
+    };
+    // update state
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div className='container'>
-      <TodoForm />
+      <TodoForm addTodoHandler={addTodoHandler} />
       <TodoList />
     </div>
   );

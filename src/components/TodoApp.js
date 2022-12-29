22383e47bb161797996e83114cc00db50cd50376
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2022-12-29 16:05:32 +0330

    Structure base in TodoApp file

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
 const TodoApp = () => {
-  return <div className='container'></div>;
+  const [todo, setTodo] = useState("");
+  return (
+    <div className='container'>
+      <TodoForm />
+      <TodoList />
+    </div>
+  );
 };
 
 export default TodoApp;

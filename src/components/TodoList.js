8bf30c3c35c858f8e56466c5db538311737ab207
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2022-12-29 17:37:13 +0330

    Refactor render todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,24 @@
 const TodoList = ({ todos }) => {
-  // if there is no todo
-  if (todos.length === 0) return <p>add note todos</p>;
+  const renderTodo = () => {
+    // if there is no todo
+    if (todos.length === 0) return <p>add note todos</p>;
+
+    return todos.map((todo) => {
+      return (
+        <div key={todo.id}>
+          <div>{todo.text}</div>
+          <div>
+            <button>Edit</button>
+            <button>Complete</button>
+          </div>
+        </div>
+      );
+    });
+  };
 
   // create structure todo list
   // loop on state
-  return (
-    <div>
-      {todos.map((todo) => {
-        return (
-          <div key={todo.id}>
-            <div>{todo.text}</div>
-            <div>
-              <button>Edit</button>
-              <button>Complete</button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <div>{renderTodo()}</div>;
 };
 
 export default TodoList;
